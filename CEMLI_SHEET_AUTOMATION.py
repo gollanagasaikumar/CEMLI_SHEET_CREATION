44b05c36b2f6bd999fdf6b24fbffa76b3552341f
gollanagasaikumar <saikumar.naga@gmail.com>
2020-03-23 17:40:51 +0530

    Update CEMLI_SHEET_AUTOMATION.py

--- CEMLI_SHEET_AUTOMATION.py
+++ CEMLI_SHEET_AUTOMATION.py
@@ -14,9 +14,14 @@ from selenium.common.exceptions import NoSuchElementException
 
 
 CREDENTIALS_LIST = ["nagasaikumar.golla@geappliances.com","515120537","NAsa@321ku"]
+print("start check")
+sys.stdout.flush()
 CEMLI_NAME = os.getenv("CEMLI_SHEET")
 #driver = webdriver.Chrome(r"C:\Users\nagasaikumar.golla\Desktop\CEMLI_SHEET_AUTOMATION_SCRIPT\chromedriver.exe")
-driver = webdriver.Chrome(r"C:\Users\nagasaikumar.golla\Desktop\CEMLI_SHEET_AUTOMATION_SCRIPT\chromedriver.exe")
+#
+chrome_options = Options()  
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(r"C:\Users\nagasaikumar.golla\Desktop\CEMLI_SHEET_AUTOMATION_SCRIPT\chromedriver.exe",chrome_options=chrome_options)
 time.sleep(3)
 driver.maximize_window()
 print("***************** CEMLI Creation Started for " + CEMLI_NAME + "*****************")
